fb84b15fad1822cab758357f1dda9e1f8c88885b
amanda-vlopes <amandavitor@hotmail.com>
2023-02-28 18:25:00 -0300

    Requisito 7 finalizado

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,26 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  const operacoes = {
+    sum: Math.floor(number1 + number2),
+    mult: Math.floor(number1 * number2),
+    div: Math.floor(number1 / number2),
+    sub: Math.floor(number1 - number2),
+  };
+  return operacoes;
+};
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  let retorno = '';
+  if (type === 'keys') {
+    retorno = Object.keys(object);
+  } else if (type === 'values') {
+    retorno = Object.values(object);
+  } else if (type === 'entries') {
+    retorno = Object.entries(object);
+  }
+  return retorno;
+};
 
 module.exports = { calculator, arrayGenerator };
