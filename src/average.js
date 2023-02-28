7c7ccf72c3c6b7956dc6e3c198f9a74e3c105ae9
amanda-vlopes <amandavitor@hotmail.com>
2023-02-28 15:11:10 -0300

    Requisito 1 finalizado

--- src/average.js
+++ src/average.js
@@ -11,6 +11,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let soma = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    soma += array[index];
+  }
+  const media = Math.round(soma / array.length);
+  return media;
+};
 
 module.exports = average;
